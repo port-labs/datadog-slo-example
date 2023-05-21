c4b49c348d1475879cdf426f1309c45e5a4d267e
pages_coffy <isaac.p.coffie@gmail.com>
2023-05-21 16:58:42 +0000

    add shell script

---
+++ app.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Get environment variables
+DATADOG_API_KEY="$DATADOG_API_KEY"
+DATADOG_APPLICATION_KEY="$DATADOG_APPLICATION_KEY"
+PORT_CLIENT_ID="$PORT_CLIENT_ID"
+PORT_CLIENT_SECRET="$PORT_CLIENT_SECRET"
+DATADOG_API_URL="https://api.us5.datadoghq.com/api/v1/"
+PORT_API_URL="https://api.getport.io/v1"
+BLUEPRINT_ID="serviceDependency"
+
+# Get Port Access Token
+credentials="{\"clientId\": \"$PORT_CLIENT_ID\", \"clientSecret\": \"$PORT_CLIENT_SECRET\"}"
+token_response=$(curl -X POST -H "Content-Type: application/json" -d "$credentials" "$PORT_API_URL/auth/access_token")
+access_token=$(echo "$token_response" | jq -r '.accessToken')
+
+# Create entity in Port
+add_entity_to_port() {
+    entity_object="$1"
+    response=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $access_token" -d "$entity_object" "$PORT_API_URL/blueprints/$BLUEPRINT_ID/entities?upsert=true&merge=true")
+    echo "$response"
+}
+
+# Retrieve service dependencies from Datadog
+retrieve_service_dependencies() {
+    env="$1"
+    headers="-H 'DD-API-KEY: $DATADOG_API_KEY' -H 'DD-APPLICATION-KEY: $DATADOG_APPLICATION_KEY' -H 'Accept: application/json'"
+    services_response=$(curl -s $headers "$DATADOG_API_URL/service_dependencies?env=$env")
+    service_dependencies=$(echo "$services_response" | jq '.')
+
+    for service in $(echo "$service_dependencies" | jq -r 'keys[]'); do
+        related_services=$(echo "$service_dependencies" | jq -r ".[\"$service\"].calls")
+        entity="{\"identifier\": \"$service\", \"title\": \"$service\", \"properties\": {}, \"relations\": {\"serviceDependency\": $related_services}}"
+        add_entity_to_port "$entity"
+    done
+}
+
+retrieve_service_dependencies "dev"
